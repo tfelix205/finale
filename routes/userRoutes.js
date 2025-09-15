72b75f58f942b1da586714c9809d066d557576f0
Daniel-Paul-chukwudi <paulchukwudidaniel@gmail.com>
2025-09-15 08:20:29 -0700

    daniel getOne

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
-const { register } = require('../controller/userController');
+const { register,getOne } = require('../controller/userController');
 const uploads = require('../middleware/multer');
 const router = require('express').Router();
 
 router.post('/register',uploads.single('profilePicture'), register,)
+router.get('/student/:id',getOne)
 
 module.exports = router;
